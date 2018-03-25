402f5da42adcf2331bac6a5570183e12563d3871
Artem Kharshan <timkharshan@hotmail.com>
2018-03-24 17:31:20 -0700

    refactoring

--- googleFilterObserver.js
+++ googleFilterObserver.js
@@ -1,29 +1,20 @@
 //makes observer that deletes new elements added to dom that match xlist filters
-let xList = ['CyberCoder','jobspring']
+let xList = ['CyberCoder']
 xList = xList.map((el)=>el.toLowerCase())
 
 const targetNode = document.body
 const config = {childList:true,subtree: true};
 const deleteFromMutation = function(mutationsList) {
-    mutationsList.forEach((mutation)=> {
-        mutation.addedNodes.forEach(node=>{
-            if (node.querySelectorAll){
-                deleteFilterFromElList(node.querySelectorAll('li'))
-            }
-        })
-    })          
+    mutationsList.forEach((mutation)=> GetLiAndFilter(mutation))          
 };
-
-// Create an observer instance linked to the callback function which deletes 
-// newly added nodes if they have text from xList
-const observer = new MutationObserver(deleteFromMutation);
-
-//intial delete 
-deleteFilterFromElList(targetNode.querySelectorAll('li'));
-// Start observing the target node for configured mutations
-observer.observe(targetNode, config);
-
-function deleteFilterFromElList(ul,filters = xList){
+function GetLiAndFilter(mutation){
+    mutation.addedNodes.forEach(node=>{
+        if (node.querySelectorAll){
+            filterFromElList(node.querySelectorAll('li'))
+        }
+    })
+}
+function filterFromElList(ul,filters = xList){
     ul.forEach( (li)=>{
         const text = li.textContent.toLowerCase()
         const match = text.match(filters)
@@ -33,4 +24,14 @@ function deleteFilterFromElList(ul,filters = xList){
         }
     })
 }
+
+// Create an observer instance linked to the callback function which deletes 
+// newly added nodes if they have text from xList
+const observer = new MutationObserver(deleteFromMutation);
+
+//intial delete 
+filterFromElList(targetNode.querySelectorAll('li'));
+// Start observing the target node for configured mutations
+observer.observe(targetNode, config);
+
 
