453b8475be118539f09d30f4efa9890541610e91
timkharshan@hotmail.com <timkharshan@hotmail.com>
2018-04-18 13:47:52 -0700

    expanded list,changed log

--- googleFilterObserver.js
+++ googleFilterObserver.js
@@ -1,32 +1,37 @@
 //makes observer that deletes new elements added to dom that match xlist filters, also does initial sweep of all LI's 
-let xList = ['oracle','creative circle','workbridge','laowaicareer','CyberCoder', 'jobspring','we work remotely','hired', 'tandem recruiting group']
-xList = xList.map((filter)=>filter.toLowerCase())
+let xList = ['oracle', 'creative circle', 'workbridge', 'laowaicareer', 'CyberCoder', 'jobspring', 'we work remotely', 'hired', 'tandem recruiting group']
+xList = xList.map((filter) => filter.toLowerCase())
 
 const targetNode = document.body
 //config obj for observer element
 // subtree true means that it will do deep check of childList change, instead of 
 // just immediate children
-const config = {childList:true,subtree: true};
+const config = {
+    childList: true,
+    subtree: true
+};
 // deleteFromMutation is a callback for the observer, which will be passed in a list of mutations. every 
 //mutation has a type, and if it is a childlist mutation is will have removed/added nodes.
 // this callback checks added nodes text for any mention of xList words and deletes them. 
-const deleteFromMutation = function(mutationsList) {
-    mutationsList.forEach((mutation)=> GetLiAndFilter(mutation))          
+const deleteFromMutation = function (mutationsList) {
+    mutationsList.forEach((mutation) => GetLiAndFilter(mutation))
 };
-function GetLiAndFilter(mutation){
-    mutation.addedNodes.forEach(node=>{
+
+function GetLiAndFilter(mutation) {
+    mutation.addedNodes.forEach(node => {
         //script nodes have querySelectorAll undefined, which throws an 'not function error' when called. 
-        if (node.querySelectorAll){
+        if (node.querySelectorAll) {
             filterFromElList(node.querySelectorAll('li'))
         }
     })
 }
-function filterFromElList(ul,filters = xList){
-    ul.forEach( (li)=>{
+
+function filterFromElList(ul, filters = xList) {
+    ul.forEach((li) => {
         const text = li.textContent.toLowerCase()
         const match = text.match(filters.join('|'))
         if (match) {
-            console.log(match,li);
+            console.log(match, li);
             li.parentNode.removeChild(li);
         }
     })
